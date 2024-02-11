54cf714abcad950793278b155a4bd3b0b44e5a87
Dilshan012 <dilshancom01@gmail.com>
2024-02-11 12:09:43 +0530

    version 1.0

--- app/src/main/java/com/example/calculator_application_android_studio/MainActivity.java
+++ app/src/main/java/com/example/calculator_application_android_studio/MainActivity.java
@@ -3,12 +3,59 @@ package com.example.calculator_application_android_studio;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button buttonAdd, buttonSub, buttonMul, buttonDiv;
+    EditText editTextN1, editTextN2;
+    TextView textView;
+
+    int num1, num2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        buttonAdd = findViewById(R.id.btn_add);
+        buttonSub = findViewById(R.id.btn_sub);
+        buttonMul = findViewById(R.id.btn_mul);
+        buttonDiv = findViewById(R.id.btn_div);
+
+        editTextN1 = findViewById(R.id.number1);
+        editTextN2 = findViewById(R.id.number2);
+        textView = findViewById(R.id.answer);
+
+        buttonAdd.setOnClickListener(this);
+        buttonSub.setOnClickListener(this);
+        buttonMul.setOnClickListener(this);
+        buttonDiv.setOnClickListener(this);
+    }
+
+    public int getIntFromEditText(EditText editText) {
+        if (editText.getText().toString().equals("")) {
+            Toast.makeText(this, "Enter Number", Toast.LENGTH_SHORT).show();
+            return 0;
+        } else
+            return Integer.parseInt(editText.getText().toString());
+    }
+
+    @Override
+    public void onClick(View view) {
+        int N1 = getIntFromEditText(editTextN1);
+        int N2 = getIntFromEditText(editTextN2);
+        if (view.getId() == R.id.btn_add) {
+            textView.setText("Answer = " + (N1 + N2));
+        } else if (view.getId() == R.id.btn_sub) {
+            textView.setText("Answer = " + (N1 - N2));
+        } else if (view.getId() == R.id.btn_mul) {
+            textView.setText("Answer = " + (N1 * N2));
+        } else if (view.getId() == R.id.btn_div) {
+            textView.setText("Answer = " + ((float) N1 / (float) N2));
+        }
     }
 }
